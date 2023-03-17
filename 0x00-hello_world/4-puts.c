5d2c6002c15b606c813ce65cc1d40630e42467b1
reem mohamed <reem2004mohamed.com>
2023-03-17 11:49:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
-
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle);
+#include <stdio.h>
+/* print Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ * return : always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
