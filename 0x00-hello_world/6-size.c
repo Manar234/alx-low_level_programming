8b35712408175f9ff001ba5280e25909a75e2112
reem mohamed <reem2004mohamed.com>
2023-03-19 11:08:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 
 /**
- * main -this is the main function
- * return 0 :when it is right 
+ * main - this is the main function
+ * print the size of a char
+ * print the size of an int
+ * print the size of a long int
+ * print the size of a long long int
+ * print the size of a float
+ * return 0 : when it is true
  */
 
 int main(void)
 {
 printf("size of a char", sizeof(char));
 printf("size of an int", sizeof(int));
-printf("size of a long int", sizeof(long int));
+printf("size of a long inti", sizeof(long int));
 printf("size of a long long int", sizeof(long long int));
 printf("size of a float", sizeof(float));
 return (0);
