49beb5b29b25a9165ffdb53fa515943fedf0b52f
reem mohamed <reem2004mohamed.com>
2023-03-19 13:37:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
  * print the size of a long int
  * print the size of a long long int
  * print the size of a float
- * return 0 : when it is true
+ * return 0 when it is true
  */
 
 int main(void)
 {
-printf("size of a char", sizeof(char));
-printf("size of an int", sizeof(int));
-printf("size of a long inti", sizeof(long int));
-printf("size of a long long int", sizeof(long long int));
-printf("size of a float", sizeof(float));
+printf("Size of a char: ", sizeof(char));
+printf("Size of an int: ", sizeof(int));
+printf("Size of a long inti: ", sizeof(long int));
+printf("Size of a long long int: ", sizeof(long long int));
+printf("Size of a float: ", sizeof(float));
 return (0);
 }
