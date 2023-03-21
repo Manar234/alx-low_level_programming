5dc601cb2d3cec49c4a01aab296dc8c893f73bc9
reem mohamed <reem2004mohamed.com>
2023-03-20 18:53:02 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+	int i = 44;
+	int u = 32;
+
+	for (c = 1; c < 10 ; c++)
+	{
+		putchar(c + 48);
+		if (c < 9)
+		{
+			putchar(i);
+			putchar(u);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
