0f98cae89b099688c4d1a51c34a1ec2fa7504b4e
reem mohamed <reem2004mohamed.com>
2023-03-20 17:36:28 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	char c;
+	char i;
 
-	for (c = 'a' ; c <= 'Z' ; c++)
-	{
+	for (c = 'a' ; c <= 'z' ; c++)
 		putchar(c);
-	}
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
