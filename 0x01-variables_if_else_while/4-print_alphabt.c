64ceaa0351aef752e75a96e5286cb9e49e1df762
reem mohamed <reem2004mohamed.com>
2023-03-20 17:47:40 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
