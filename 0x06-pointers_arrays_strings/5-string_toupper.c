3838406f3222e56926a653fa3abf1af83395f90c
reem mohamed <reem2004mohamed.com>
2023-03-30 06:23:45 -0700

    5-string_upper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: string to converted
+ * Return: strings
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (0);
+}
