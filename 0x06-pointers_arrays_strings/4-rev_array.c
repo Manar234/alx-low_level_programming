5644cde0751a4e4d9bddaa0c121dcb12ef271426
reem mohamed <reem2004mohamed.com>
2023-03-30 06:12:03 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @n: function parameter
+ * @a: function parmeter
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < n / 2 ; i++)
+	{
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
+}
