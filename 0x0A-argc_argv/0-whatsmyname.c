e60599d10af11145d7c1deb66b2b483b5f743a5b
reem mohamed <reem2004mohamed.com>
2023-04-08 00:02:16 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the program name, followed by a new line
+ * @agrc: int
+ *@argv: list
+ * Return: 0
+ */
+int main(int agrc, char const *argv[])
+{
+	(void)agrc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
