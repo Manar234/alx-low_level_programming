23c5f4d80f28b21b8f4eddd848e98bc8ed7a014c
reem mohamed <ermohamed287@gmail.com>
2023-06-06 23:17:32 -0700

    4

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		printf("\n");
+		_putchar('\n');
 	}
 	else
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
 }
