78c36fe3701043d2e463b685ad6f6f41e9cb08e4
reem mohamed <ermohamed287@gmail.com>
2023-06-07 16:12:28 -0700

    recurion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,10 +3,35 @@
  * wildcmp - that compares two strings and returns 1 if the strings can
  * be considered identical, otherwise return 0;
  * @s1: string one
+ * pre - function to compare two strings
  * @s2: string two
  * Return: 1 or 0
  */
-int pre(const char *c, int s, int e);
+int pre(char *s1, char *s2);
 int wildcmp(char *s1, char *s2)
 {
-	return (pre(
+	return (pre(s1, s2));
+}
+/**
+ * pre - function to compare two strings
+ * @s1: pointer to string one
+ * @s2: pointer to string two
+ * Return: 1 if the strings can be considered identical,
+ * 0 otherwise
+ */
+int pre(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 == '\0')
+			return (pre(s1, s2 + 1));
+		return (pre(s1 + 1, s2) || pre(s1, s2 + 1));
+	}
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (*s1 == *s2);
+	else if (*s1 == *s2 || *s2 == '?')
+		return (pre(s1 + 1, s2 + 1));
+				return (0);
+}
