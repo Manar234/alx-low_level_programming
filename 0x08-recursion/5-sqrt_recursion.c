b24f4a0c8bc5f6e6a1c24f28bf085fe5be508fa4
reem mohamed <ermohamed287@gmail.com>
2023-06-07 01:45:54 -0700

    6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,27 +4,23 @@
  * @n: input integer
  * Return: results
  */
+int squre(int n, int val);
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n < 0)
-		return (-1);
-	if (n ==  1 || n == 0)
-		return (n);
-	i = 1;
-	while (i * i <= n)
-	{
-		i++;
-	}
-	i--;
-
-	if (i * i == n)
-	{
-		return (i);
-	}
+	return (squre(n, 1));
+}
+/**
+ * squre - return natural squre root
+ * @n: int
+ * @val: qqure root
+ * Return: int
+ */
+int squre(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (squre(n, val + 1));
 	else
-	{
 		return (-1);
-	}
 }
